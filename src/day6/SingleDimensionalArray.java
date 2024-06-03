c86f362550bf5163c03cee0842c10c01db03eca6
mahil1 <mahil.sardana806@gmail.com>
2024-06-03 10:51:06 +0530

    updated Array Code

---
+++ src/day6/SingleDimensionalArray.java
@@ -0,0 +1,29 @@
+package day6;
+
+/*
+1 Declare an array
+2 add values into array
+3 find size of an array
+4 read single value from an array
+5 read multiple values from an array
+*/
+public class SingleDimensionalArray {
+
+	public static void main(String[] args) {
+		
+		//1 declare an array
+		//Approach-1
+		int a[]=new int[5];
+		
+		//2 add values into array
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		a[3]=40;
+		a[4]=50;
+		
+		
+
+	}
+
+}
